e4c7f31a9ccd87e1f3f86602be25c77b2daf4973
outiti88 <outiti.ayoub@gmail.com>
2020-12-26 17:06:53 +0100

    components+serices+model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddPatientComponent } from './components/add-patient/add-patient.component';
+import { EditPatientComponent } from './components/edit-patient/edit-patient.component';
+import { ListPatientComponent } from './components/list-patient/list-patient.component';
+import { NavbarComponent } from './components/partials/navbar/navbar.component';
+import { NotfoundComponent } from './components/partials/notfound/notfound.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPatientComponent,
+    EditPatientComponent,
+    ListPatientComponent,
+    NavbarComponent,
+    NotfoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
