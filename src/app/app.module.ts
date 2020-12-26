a463f844dd81c48aa27311f756b5c5467a8ed499
outiti88 <outiti.ayoub@gmail.com>
2020-12-26 17:52:00 +0100

    routing system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router' ;
 import { AppComponent } from './app.component';
 import { AddPatientComponent } from './components/add-patient/add-patient.component';
 import { EditPatientComponent } from './components/edit-patient/edit-patient.component';
@@ -8,6 +8,27 @@ import { ListPatientComponent } from './components/list-patient/list-patient.com
 import { NavbarComponent } from './components/partials/navbar/navbar.component';
 import { NotfoundComponent } from './components/partials/notfound/notfound.component';
 import { LoginComponent } from './components/login/login.component';
+
+
+
+const routes: Routes =[
+  {
+    path : "" , redirectTo : '/patient' , pathMatch : 'full'
+  },
+
+  { path : "patient" , children:[
+    { path : " " , component : ListPatientComponent},
+    { path : ":id/edit " , component : EditPatientComponent},
+    { path : "add" , component : AddPatientComponent}
+  ]} , 
+  {
+    path : "login" , component : LoginComponent
+  } ,
+  {
+    path : "**" , component : NotfoundComponent
+  }
+] ;
+
 
 @NgModule({
   declarations: [
@@ -20,6 +41,7 @@ import { LoginComponent } from './components/login/login.component';
     LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
