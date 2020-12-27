38a4a7e2ea06131373f933d4255ece0847d531e4
outiti88 <outiti.ayoub@gmail.com>
2020-12-27 17:59:39 +0100

    token in local storage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  login(data : {email : string, password : string}){
+    return this.http.post("http://localhost:8085/login",data);
+  }
+
 }
